0f2148df88519a8b71fcc2e7b6b442f7888df1a6
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-04-27 13:49:52 -0300

    Update Esteira.java

--- ProjetoDaEsteira/src/Esteira.java
+++ ProjetoDaEsteira/src/Esteira.java
@@ -1,17 +1,18 @@
-import java.util.LinkedList;
+
+import java.util.List;
 public class Esteira {
 	public Esteira() {
 		
 	}
 	
-	public boolean porNaCaixa(LinkedList<Produto> pacote) {
+	public boolean porNaCaixa(List<Produto> pacote) {
 		if(pacote.size() == 0) {
 			System.out.println("Pacote vazio");
-			return false;
+			return true;
 		} else {
 			System.out.println("Colocou o pacote de "+ pacote.get(0).getFornecedor() +" na caixa");
 			pacote.removeAll(pacote);
-			return true;
+			return false;
 		}
 	}
 }
