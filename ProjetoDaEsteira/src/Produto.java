b360a1f586be6f23e979aad2b57a232b45942d10
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-04-27 16:14:13 -0300

    compareTo

--- ProjetoDaEsteira/src/Produto.java
+++ ProjetoDaEsteira/src/Produto.java
@@ -1,5 +1,5 @@
 
-public class Produto {
+public class Produto  implements Comparable<Produto>{
 
 	private String fornecedor;
 	private int totalProdutos;
@@ -17,17 +17,28 @@ public class Produto {
 		this.prazo = prazo;
 	}
 
+	//converte cada linha do CSV em um novo  produto
 	public static Produto converterLinhaCSVEmProduto(String linha, String separador) {
 		String[] partes = linha.split(separador);
 		if(partes.length != 4) {
 			throw new IllegalArgumentException("Linha de produto está incorreta: " + linha);
-			
 		}
 		try {
 			return new Produto(partes[0].trim(), Integer.parseInt(partes[1].trim()), Integer.parseInt(partes[2].trim()), Integer.parseInt(partes[3].trim()));
 		} catch(NumberFormatException e) {
 			throw new IllegalArgumentException("Linha de produto está incorreta: "+ linha);
 		}
+	}
+	
+	@Override
+	public int compareTo(Produto produto) {
+		if(this.prazo < produto.getPrazo()) {
+			return -1;
+		}
+		if (this.prazo > produto.getPrazo()) {
+			return 1;
+		}
+		return 0;
 	}
 
 	public String getFornecedor() {
